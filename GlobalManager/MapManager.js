34f3e47166bc26572529fe26307a1caf8d529a60
berniehuang2008 <berniehuang2008@163.com>
2024-11-29 20:18:17 +0800

    重构 MapManager 类，移除 ID 记录，改用对象反向索引管理坐标

--- GlobalManager/MapManager.js
+++ GlobalManager/MapManager.js
@@ -22,24 +22,8 @@ class MapManager {
         this.w_cell = Math.ceil(w / BOARDMAP_SECTOR_SIZE);
         this.h_cell = Math.ceil(h / BOARDMAP_SECTOR_SIZE);
 
-        this.id_to_func = new Map();
-        this.id_to_coord = new Map();
+        this.map_revidx = new Map();    // reverse index
         this.map = ndArray(this.w_cell, this.h_cell, () => { return new Set() });
-
-    }
-
-    genid(func) {
-        // gen id
-        var id = Math.random();
-        while (this.id_to_func.has(id)) {
-            id = Math.random();
-        }
-
-        // add to record
-        this.id_to_func.set(id, func);
-        this.id_to_coord.set(id, []);
-
-        return id;
     }
 
     _calc_cell(x, y) {
@@ -49,46 +33,50 @@ class MapManager {
         return [x_cell, y_cell];
     }
 
-    add(id, x, y) {
+    add(obj, x, y) {
         var [x_cell, y_cell] = this._calc_cell(x, y);
 
-        // record
-        this.id_to_coord.get(id).push([x_cell, y_cell]);
-        this.map[x_cell][y_cell].add(obj);
+        if (this.map_revidx.has(obj)) {
+            this.map_revidx[obj].push([x_cell, y_cell]);
+            this.map[x_cell][y_cell].add(obj);
+        }
+        else {
+            this.map_revidx.set(obj, [[x_cell, y_cell]]);
+            this.map[x_cell][y_cell].add(obj);
+        }
 
         return true;
     }
 
-    remove(id) {
-        if (!this.id_to_func.has(id))
+    remove(obj) {
+        if (!this.map_revidx.has(obj))
             return false;
 
         // remove from main map
-        for (c of this.id_to_coord.get(id)) {
+        for (c of this.map_revidx.get(obj)) {
             var [x_cell, y_cell] = c;
-            
-            this.map[x_cell][y_cell].remove(id);
+
+            this.map[x_cell][y_cell].remove(obj);
         }
 
         // remove from id record
-        this.id_to_func.remove(id);
-        this.id_to_coord.remove(id);
+        this.map_revidx.remove(obj);
 
         return true;
     }
 
-    remove(id, x, y) {
+    remove(obj, x, y) {
         var [x_cell, y_cell] = this._calc_cell(x, y);
 
         // remove from map
-        var res = this.map[x_cell][y_cell].remove(id);
+        var res = this.map[x_cell][y_cell].remove(obj);
         if (!res)
             return false;
 
         // remove record
-        this.id_to_coord.remove([x_cell, y_cell]);
-        if (this.id_to_coord.size == 0) {
-            this.remove(id);
+        this.map_revidx.remove([x_cell, y_cell]);
+        if (this.map_revidx.size == 0) {
+            this.remove(obj);
         }
 
         return true;
@@ -98,8 +86,8 @@ class MapManager {
         var [x_cell, y_cell] = this._calc_cell(x, y);
 
         var res = [];
-        for(id of this.map[x_cell, y_cell]) {
-            res.push(this.id_to_func.get(id));
+        for (obj of this.map[x_cell, y_cell]) {
+            res.push(obj);
         }
         return res;
     }
@@ -107,15 +95,15 @@ class MapManager {
     gets(lst) {
         var res = new Set();
 
-        for(c of lst) {
+        for (c of lst) {
             var [x_cell, y_cell] = this._calc_cell(c[0], c[1]);
 
             res = res.union(this.map[x_cell][y_cell]);
         };
 
         var res_list = [];
-        for (id of res) {
-            res_list.push(this.id_to_func(id));
+        for (obj of res) {
+            res_list.push(obj);
         }
 
         return res_list;
